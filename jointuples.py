cf244289b05de8662fdfeb325bd4d494f1a2743d
venkatasaikrishnag <79572215+venkatasaikrishnag@users.noreply.github.com>
2021-09-18 16:16:39 +0530

    Add files via upload

---
+++ jointuples.py
@@ -0,0 +1,13 @@
+
+test_list = [(5, 6), (5, 7), (6, 8), (6, 10), (7, 13)]
+  
+print("The original list is : " + str(test_list))
+  
+res = []
+for sub in test_list:                                           
+    if res and res[-1][0] == sub[0]:              
+        res[-1].extend(sub[1:])                        
+    else:
+        res.append([ele for ele in sub])     
+res = list(map(tuple, res)) 
+print("The extracted elements : " + str(res)) 
